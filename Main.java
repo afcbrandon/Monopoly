d760364a0c8603248ab2af7b85b78b40f74a3e41
afcbrandon <128235286+afcbrandon@users.noreply.github.com>
2025-02-28 20:56:24 -0600

    Concise code in Main.java

--- Main.java
+++ Main.java
@@ -8,29 +8,5 @@ public class Main {
   private void go() {
     UserInterface ui = new UserInterface();
     ui.start();
-
-    /*
-
-    while (numberOfPlayers < 2 || numberOfPlayers > 8) {
-      String input = JOptionPane.showInputDialog("Enter number of players (2-8):");
-      try {
-        numberOfPlayers = Integer.parseInt(input);
-        if (numberOfPlayers < 2 || numberOfPlayers > 8) {
-          JOptionPane.showMessageDialog(null, "Please enter a number between 2 and 8");
-        }
-      } catch (NumberFormatException e) {
-        JOptionPane.showMessageDialog(null, "Invalid input. Please enter a number.");
-      }
-    }
-
-    // Create players array based on user input
-    Player[] players = new Player[numberOfPlayers];
-    for (int i = 0; i < numberOfPlayers; i++) {
-      players[i] = new Player("Player " + (i + 1));
-    }
-      
-    new GameGUI(players);
-
-    */
   }
 }
