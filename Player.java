21be3e4560d8db2ed166040e55b3c639ab6f6a48
afcbrandon <128235286+afcbrandon@users.noreply.github.com>
2025-03-26 11:50:05 -0500

    Cleaned the code up a bit, and made it more object oriented.

--- Player.java
+++ Player.java
@@ -9,6 +9,8 @@ public class Player {
     private char playerToken;
     private boolean isJailed;
     private int jailCounter;
+    private boolean diceDouble;
+    private int diceDoubleCounter;
 
     private GameBoardSpaces gameBoardSpaces;
 
@@ -20,61 +22,78 @@ public class Player {
         this.isJailed = false;
         this.jailCounter = 0;
         this.gameBoardSpaces = gameBoardSpaces;
+        this.diceDouble = false;
+        this.diceDoubleCounter = 0;
     }
 
+    /*
+        GETTERS
+     */
+    // Function that returns the player's name
     public String getName() {
         return this.name;
     }
-
+    // function that returns the player's current money
     public int getMoney() {
         return this.money;
     }
-
+    // Function that returns the player's position on the board
     public int getPosition() {
         return this.position;
+    }
+    // ***BRANDON*** Function that sets the player token
+    public void setToken(Character token) {
+        this.playerToken = token;
+    }
+    // ***BRANDON*** Function the returns the player token
+    public Character getToken() {
+        return this.playerToken;
+    }
+    // Function that returns the boolean value of diceDouble
+    public boolean getDiceDouble() {
+        return this.diceDouble;
+    }
+
+    /*
+        Setters
+     */
+    public void setMoney(int money) {
+        this.money = money;
     }
 
     public boolean isEliminated() {
         return isEliminated;
     }
 
-    // ***BRANDON*** Function that sets the player token
-    public void setToken(Character token) {
-        this.playerToken = token;
-    }
-
-    // ***BRANDON*** Function the returns the player token
-    public Character getToken() {
-        return this.playerToken;
-    }
-
-    public void moveSpaces(int spaces) {
-        position = (position + spaces) % 40; // Monopoly board has 40 spaces
+    /* Function that moves the player's token along the board */
+    public void moveSpaces(int rolledAmount) {
+        position = (position + rolledAmount) % 40; // Monopoly board has 40 spaces
         if (position == 0) {    // 40 % 40 = 0, but 40th space is valid
-            position = 40;
+            this.position = 40;
         }
-
     }
 
+    /* Function that update's the player's available money */
     public void updateMoney(int amount) {
         this.money += amount;
-        if (this.money < 0) {
+        if (this.money < 1) {
             this.isEliminated = true; // Mark player as eliminated
         }
     }
     
     /* Function that is called at the start of a player's turn */
     public void playerTurn() {
+        // TODO: Check to see if this section of code is even necessary
         if (isEliminated) {
             JOptionPane.showMessageDialog(null, this.name + " is eliminated and cannot roll.");
             return;
         }
 
         // Player has option to leave jail, if jailed
-        if (isJailed) {
+        if (this.isJailed) {
             getOutOfJail();
         } else {
-            rollDice();
+            playerRoll();
         }
 
     }
@@ -100,19 +119,30 @@ public class Player {
 
         // Player selects option to roll doubles
         if (userOption.equals("A")) {
-            Random rand = new Random();
-
-            int diceOne = rand.nextInt(6) + 1;
-            int diceTwo = rand.nextInt(6) + 1;
+            int diceOne = diceRoll();
+            int diceTwo = diceRoll();
             int result = diceOne + diceTwo;
 
+            /* If the player successfully rolls double, then the player leaves jail, and the jail counter resets. */
             if (diceOne == diceTwo) {
                 this.isJailed = false;
                 moveSpaces(result);
-
+                this.jailCounter = 0;
             }
-            else {
-                this.jailCounter += 1;  // increment jail counter, meaning that the player failed to roll doubles
+            else {  /* If the player fails to roll doubles, then the jailCounter increments by 1. */
+                this.jailCounter += 1;
+            }
+
+            if (jailCounter == 3) { // Player has failed to roll a double after 3 turns. They must pay $50 and move the amount of spaces they rolled
+                updateMoney(-50);
+                
+                if (this.money <= 0) { // Player has gone bankrupt and they are eliminated
+                    this.isEliminated = true;
+                } else {    // Player leaves jail and moves the amount they previously rolled
+                    this.isJailed = false;
+                    this.jailCounter = 0;
+                    moveSpaces(result);
+                }
             }
         }
 
@@ -124,48 +154,44 @@ public class Player {
         // TODO: Implement a 'Get Out of Jail card'
         // **** CODE **** //
     }
-
-    // Function that allows user to roll dice.
-    private void rollDice() {
-
-        // variables that check to see if user has rolled doubles. 
-        boolean rolledDoubles;
-        int doubleCounter = 0;
-
-        do {
-            rolledDoubles = false;  // boolean to check if doubles are rolled is set to false at the start of each loop
-            Random rand = new Random();
-
-            int diceOne = rand.nextInt(6) + 1;
-            int diceTwo = rand.nextInt(6) + 1;
-            int result = diceOne + diceTwo;
-            JOptionPane.showMessageDialog(null, this.name + " rolled a "  + diceOne + " and a " + diceTwo + 
-                " summing up for a total of " + result, "Dice Roll", JOptionPane.INFORMATION_MESSAGE);
-            int previousPosition = this.position;
-            this.moveSpaces(result);
-
-            // **ALEX**This will check if a player landed on a property and pay rent if needed(hopefully have not tested it yet)
-            if(gameBoardSpaces.isProperty(this.position)) {
-                gameBoardSpaces.payRent(this, this.position); //pay rent to the property owner
-            }
-
-            if (diceOne == diceTwo) {
-                rolledDoubles = true;
-                doubleCounter += 1;
-            }
-
-            // If statement that increments the rolled double counter. If player rolls double 3 times, then the player is sent to jail.
-            if (doubleCounter == 3) {
-                this.isJailed = true;
-                // TODO: Create a function that updated the player's position to the jail space
-                break;
-            }
-
-            if (!isJailed) {    // Check if player has passed go only if player has not been jailed. Player in jail cannot collect GO money
-                checkPassedGo(previousPosition); // added this into the roll and move
-            }
-
-        } while (rolledDoubles);
+  
+    /* Function that rolls a six-sided-die and returns its value */
+    private int diceRoll() {
+        return new Random().nextInt(6) + 1;
     }
-    
+
+    /* Function where player rolls the two dice */
+    private void playerRoll() {
+        int previousPosition = this.position;
+        int diceOne = diceRoll();
+        int diceTwo = diceRoll();
+        int rollResult = diceOne + diceTwo;
+
+        JOptionPane.showMessageDialog(null, this.name + " rolled a "  + diceOne + " and a " + diceTwo + 
+                " summing up for a total of " + rollResult, "Dice Roll", JOptionPane.INFORMATION_MESSAGE);
+        moveSpaces(rollResult);
+
+        checkPassedGo(previousPosition);
+
+        // **ALEX**This will check if a player landed on a property and pay rent if needed(hopefully have not tested it yet)
+        if(gameBoardSpaces.isProperty(this.position)) {
+            gameBoardSpaces.payRent(this, this.position); //pay rent to the property owner
+        }
+
+        if (diceOne == diceTwo) {
+            this.diceDouble = true;
+            this.diceDoubleCounter += 1;
+        }
+        else {
+            this.diceDoubleCounter = 0;
+            this.diceDouble = false;
+        }
+
+        if (this.diceDoubleCounter == 3) {
+            JOptionPane.showMessageDialog(null, 
+                this.name + " has rolled doubles 3 times in a row! GO TO JAIL!");
+            this.isJailed = true;
+            this.position = 10;
+        }
+    }
 }
