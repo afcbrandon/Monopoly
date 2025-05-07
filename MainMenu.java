9bc4e9698502771859166817090a10ecfab25e42
Alexander <alexvertiz7@gmail.com>
2025-05-07 16:39:35 -0500

    Made it where you can play with Bots

---
+++ MainMenu.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenu extends JFrame {
+    public MainMenu() {
+
+        setTitle("Monopoly Main Menu");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new GridLayout(3, 1));
+
+        JButton playHumanBtn = new JButton("Play with Human Players");
+        JButton playBotBtn = new JButton("Play with Bot(s)");
+        JButton exitBtn = new JButton("Exit");
+
+        playHumanBtn.addActionListener(e -> {
+            dispose();  // Close this window
+            new UserInterface(false).start(); // false = human players only
+        });
+
+        playBotBtn.addActionListener(e -> {
+            dispose();
+            new UserInterface(true).start(); // true = play with bots
+        });
+
+        exitBtn.addActionListener(e -> System.exit(0));
+
+        add(playHumanBtn);
+        add(playBotBtn);
+        add(exitBtn);
+
+        setVisible(true);
+    }
+}
